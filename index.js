995692d483ae7c750601e40026a840bdaa81a39a
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-08 23:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string).toUpperCase();
+  console.log("HELLO").toUpperCase();
 }
 function logWhisper(string) {
   console.log("HELLO").toLowerCase();
